c345b95ff84fa362ea7296188d6fdbed685e119e
Dream <2965106733@qq.com>
2025-06-10 22:37:17 +0800

    original

---
+++ src/main/java/com/dream/softwarecupspring/SoftWareCupSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dream.softwarecupspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftWareCupSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftWareCupSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dream/softwarecupspring/SoftWareCupSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.softwarecupspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftWareCupSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
