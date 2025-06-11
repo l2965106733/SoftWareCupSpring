69bcf67da906847ecf51a55c723b0dc2bf2d91b0
Dream <2965106733@qq.com>
2025-06-11 20:45:31 +0800

    2025.6.11

--- src/main/java/com/dream/softwarecupspring/pojo/user.java
+++ src/main/java/com/dream/softwarecupspring/pojo/user.java
@@ -1,7 +1,15 @@
 package com.dream.softwarecupspring.pojo;
 
-public class user implements Serializable {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class user{
         private Long id;               // 用户唯一ID
         private String username;       // 用户名
         private String password;       // 加密后的密码
